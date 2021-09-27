c35b30a03bb6b263c7640255dd394e9037bd3d80
MusicDev33 <smccowan33@gmail.com>
2021-09-26 20:01:25 -0600

    Rust ping blink

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use tokio_amqp::*;
+use std::time::Duration;
 use lapin::{
   Connection, ConnectionProperties, types::FieldTable, options::*
 };
 use std::sync::Arc;
 use tokio::runtime::Runtime;
 use futures_util::stream::StreamExt;
+use std::process::Command;
+use std::path::Path;
+
+use tokio::time::sleep;
+use std::io::Write;
+use std::fs::OpenOptions;
 
 async fn tokio_main(rt: Arc<Runtime>) {
   let url = "amqp://localhost";
@@ -21,6 +28,38 @@ async fn tokio_main(rt: Arc<Runtime>) {
     println!("{:?}", std::str::from_utf8(&delivery.data).unwrap());
 
     delivery.ack(BasicAckOptions::default()).await.unwrap();
+
+    let filePath = "/sys/class/leds/led0/brightness";
+
+    let fileExists = Path::new(filePath).exists();
+
+    if !fileExists {
+      break;
+    }
+
+    for _ in 0..10 {
+      let interval = Duration::from_millis(15);
+      sleep(interval).await;
+      let mut file = OpenOptions::new().write(true).truncate(true).open(filePath).unwrap();
+      file.write_all(b"1").expect("Could not write to brightness file.");
+      file.flush().expect("Could not flush brightness file.");
+      
+      sleep(interval).await;
+      file.write_all(b"0").expect("Could not write to brightness file.");
+      file.flush().expect("Could not flush brightness file.");
+    }
+
+    for _ in 0..6 {
+      let interval = Duration::from_millis(50);
+      sleep(interval).await;
+      let mut file = OpenOptions::new().write(true).truncate(true).open(filePath).unwrap();
+      file.write_all(b"1").expect("Could not write to brightness file.");
+      file.flush().expect("Could not flush brightness file.");
+
+      sleep(interval).await;
+      file.write_all(b"0").expect("Could not write to brightness file.");
+      file.flush().expect("Could not flush brightness file.");
+    }
   }
 
 }
--- src/parser.ts
+++ src/parser.ts
@@ -35,6 +35,6 @@ export const parseMessage = async (msg: string) => {
   }
 
   if (task === 'pingblink') {
-    await pingBlink();
+    // await pingBlink();
   }
 }
