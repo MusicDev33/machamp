6c9c1d0ae519d054fec4b2840d6d6a2bf128ccb6
MusicDev33 <smccowan33@gmail.com>
2021-11-07 20:47:28 -0700

    conventions

--- src/jobs/ping_blink.rs
+++ src/jobs/ping_blink.rs
@@ -6,18 +6,18 @@ use std::io::Write;
 use std::fs::OpenOptions;
 
 pub async fn ping_blink() {
-  let filePath = "/sys/class/leds/led0/brightness";
+  let file_path = "/sys/class/leds/led0/brightness";
 
-  let fileExists = Path::new(filePath).exists();
+  let file_exists = Path::new(file_path).exists();
 
-  if !fileExists {
+  if !file_exists {
     return;
   }
 
   for _ in 0..10 {
     let interval = Duration::from_millis(30);
     sleep(interval).await;
-    let mut file = OpenOptions::new().write(true).truncate(true).open(filePath).unwrap();
+    let mut file = OpenOptions::new().write(true).truncate(true).open(file_path).unwrap();
     file.write_all(b"1").expect("Could not write to brightness file.");
     file.flush().expect("Could not flush brightness file.");
     
@@ -29,7 +29,7 @@ pub async fn ping_blink() {
   for _ in 0..6 {
     let interval = Duration::from_millis(60);
     sleep(interval).await;
-    let mut file = OpenOptions::new().write(true).truncate(true).open(filePath).unwrap();
+    let mut file = OpenOptions::new().write(true).truncate(true).open(file_path).unwrap();
     file.write_all(b"1").expect("Could not write to brightness file.");
     file.flush().expect("Could not flush brightness file.");
 
