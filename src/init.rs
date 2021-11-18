5346de50aa9475e9b286a94b9ee5dbfd0ee099ff
MusicDev33 <smccowan33@gmail.com>
2021-11-18 00:24:57 -0700

    Better test

--- src/init.rs
+++ src/init.rs
@@ -2,6 +2,7 @@ use std::process::Command;
 
 pub fn initialize() {
   let stdout = Command::new("redis-cli").args(["ping"]).output().expect("Redis ping failed").stdout;
-  let text = String::from_utf8(stdout).expect("Invalid UTF-8");
+  let mut text = String::from_utf8(stdout).expect("Invalid UTF-8");
+  text.pop();
   println!("{:?}", text.to_owned());
 }
